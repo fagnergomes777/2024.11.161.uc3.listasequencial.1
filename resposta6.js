975c6989b66c292f8d90941717a653308a0a7683
fagnergomes777 <phagner@outlook.com>
2024-09-23 19:35:24 -0300

    Update resposta6.js

--- resposta6.js
+++ resposta6.js
@@ -13,14 +13,3 @@ const area = Math.PI * (raio ** 2)
 
 // mostrando a área do círculo
 console.log(`A área do círculo é ${area}`)
-
-
-// Faça um algoritmo que peça o raio de um círculo, calcule e mostre sua área
-
-const prompt = require("prompt-sync")()
-
-const raio = parseFloat(prompt("Digte o raio do círculo:"))
-
-const area = cacularAreaCirculo(raio)
-
-console.log (`A área do círculo com raio ${raio} é ${area.toFixed(2)}`)
