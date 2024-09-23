5d80f6c35dd34318b660e3ff5d18c7b5a7ccf294
fagnergomes777 <phagner@outlook.com>
2024-09-23 19:33:09 -0300

    Update resposta9.js

--- resposta9.js
+++ resposta9.js
@@ -1,4 +1,24 @@
 // Faça um algoritmo que peça a temperatura em graus Fahrenheit, transforme e mostre a 
+// temperatura em graus Celsius.
+(C = 5 * ((F-32) / 9)).
+
+// definindo a constante prompt
+const prompt = require('prompt-sync')()
+
+// pegando a temperatura em fahrenheit
+const fahrenheit = promot(`Digite a temperatura em fahrenheit: `)
+
+// convertendo a temperatura em numero
+fahrenheit = parseFloat(fahrenheit)
+
+// calculando a temperatura em celsius
+const celsius = 5 * ((fahrenheit - 32) / 9)
+
+// mostrando a temperatura em cesius
+console.log (`A temperatura em celsius é: ${celsius.toFixed(2)}`)
+
+
+// Faça um algoritmo que peça a temperatura em graus Fahrenheit, transforme e mostre a 
 // temperatura em graus Celsius. (C = 5 * ((F-32) / 9)).
 
 const prompt = require("prompt-sync")()
