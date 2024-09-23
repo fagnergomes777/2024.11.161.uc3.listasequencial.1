10e6848fe41958400dc02cdbc78fbc18e5a08e24
fagnergomes777 <phagner@outlook.com>
2024-09-23 19:35:45 -0300

    Update resposta9.js

--- resposta9.js
+++ resposta9.js
@@ -16,15 +16,3 @@ const celsius = 5 * ((fahrenheit - 32) / 9)
 
 // mostrando a temperatura em cesius
 console.log (`A temperatura em celsius é: ${celsius.toFixed(2)}`)
-
-
-// Faça um algoritmo que peça a temperatura em graus Fahrenheit, transforme e mostre a 
-// temperatura em graus Celsius. (C = 5 * ((F-32) / 9)).
-
-const prompt = require("prompt-sync")()
-
-const fahrenheit = parseFloat(prompt("Digite a temperatura em graus Fahrenheit:"))
-
-const celsius = fahrenheitParaCelsius(fahrenheit)
-
-console.log(`A temperatura em graus Celsius é ${celsius.toFixed(2)}°c.`)
